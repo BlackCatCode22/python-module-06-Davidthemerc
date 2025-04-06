762cc47253ae8bb20bc0fde42f18d7d87c203de0
David Mercado <dmerc90@gmail.com>
2025-04-06 08:05:11 -0700

    Added various other methods for fun

--- inheritance.py
+++ inheritance.py
@@ -28,13 +28,40 @@ class FootballFan(PartyAnimal):
         self.points = self.points + 2
         print(self.name,"TWO POINT CONVERSION IS GOOD! Points:",self.points)
 
-s = PartyAnimal("Sally")
+class FootballTeam(FootballFan):
+
+    def __init__(self,nam):
+        super().__init__(nam)
+        self.points = 0
+
+    def fairweather(self):
+        self.x = self.x - 1
+        print(self.name,"- A party member stopped watching the game. Good job! New Party Count:",self.x)
+
+    # Honestly this should be -1 point in the NFL
+    def doink(self):
+        self.points = self.points + 0
+        print(self.name,"- The kicker doinks the kick! No!")
+
+    def intercepted(self):
+        print(self.name,"threw an INTERCEPTION! Turnover!")
+
+s = PartyAnimal("Sally's Team")
 s.party()
 
-j = FootballFan("Jim")
+j = FootballFan("Jim's Team")
 j.party()
 j.touchdown()
 j.extrapoint()
 
 j.touchdown()
 j.twopointconversion()
+
+b = FootballTeam("Caleb's Team")
+b.party()
+b.party()
+b.party()
+b.touchdown()
+b.doink()
+b.fairweather()
+b.intercepted()
