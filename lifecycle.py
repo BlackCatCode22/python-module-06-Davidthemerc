cfa5c4028fea0a09d1ee7b1aefd34ac2b2acb463
David Mercado <dmerc90@gmail.com>
2025-04-05 17:45:46 -0700

    Initial commit of lifecycle.py

---
+++ lifecycle.py
@@ -0,0 +1,18 @@
+class partyAnimal:
+
+    def __init__(self):
+        self.x = 0
+        print('I am constructed')
+
+    def party(self) :
+            self.x = self.x + 1
+            print ('So far',self.x)
+
+    def __del__(self):
+                print('I am destructed',self.x)
+
+an = partyAnimal()
+an.party()
+an.party()
+an = 42
+print('an contains',an)
